fc889e53772e3297a9919800d50f7749cdac796a
alissonbsi <bsi.alisson@gmail.com>
2020-01-06 19:37:46 -0300

    Testes UserCOntroller

---
+++ src/main/java/com/wallet/controllers/UserControle.java
@@ -0,0 +1,61 @@
+package com.wallet.controllers;
+
+import javax.validation.Valid;
+
+import org.hibernate.internal.ExceptionConverterImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wallet.dto.UserDTO;
+import com.wallet.entity.User;
+import com.wallet.response.Response;
+import com.wallet.service.UserService;
+
+@RestController
+@RequestMapping("user")
+public class UserControle {
+
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping
+	public ResponseEntity<Response<UserDTO>> create(@Valid @RequestBody UserDTO userDto, BindingResult result ) throws Exception{
+		
+		Response<UserDTO> response = new Response<UserDTO>();
+		
+		User user = userService.save(this.convertDtoToEntity(userDto));
+		
+		response.setData(this.convertEntityToDto(user));
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
+	
+	/**
+	 * 
+	 * @param dto
+	 * @return USER
+	 */
+	private User convertDtoToEntity(UserDTO dto) {
+		User user = new User();
+		user.setEmail(dto.getEmail());
+		user.setName(dto.getName());
+		user.setPassword(dto.getPassword());
+		
+		return user;
+	}
+	
+	private UserDTO convertEntityToDto(User user) {
+		UserDTO userDto = new UserDTO();
+		userDto.setEmail(user.getEmail());
+		userDto.setName(user.getName());
+		userDto.setPassword(user.getPassword());
+		
+		return userDto;
+	}
+}
---
+++ src/main/java/com/wallet/dto/UserDTO.java
@@ -0,0 +1,55 @@
+package com.wallet.dto;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+public class UserDTO {
+	
+	private Long id;
+	private String email;
+	private String name;
+	private String password;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	@Email(message = "Email inválido!")
+	@NotEmpty(message = "Email não pode ser vazio.")
+	@Length(min = 5, max = 200, message = "Email deve conter entre 5 e 200 caracteres.")
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@NotEmpty(message = "Nome não pode ser vazio.")
+	@Length(min = 3, max = 200, message = "Nome deve conter entre 3 e 200 caracteres.")
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	@NotEmpty(message = "Senha não pode ser vazia.")
+	@Length(min = 6, max = 8, message = "Nome deve conter entre 3 e 8 caracteres.")
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	
+
+}
